b4d7f0d8f7e1aea6e70f6095a58004db427fa854
Leo Fontes <79946719+LeoFontez@users.noreply.github.com>
2022-12-27 18:53:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,30 +8,59 @@ function rand(min, max) {
 function esperar(msg, tempo) {
     return new Promise((resolve, reject) => {
 
-        if (typeof msg !== 'string') reject('BAD VALUE');
+        if (typeof msg !== 'string') {
+            reject('BAD VALUE');
+            return;
+        }
 
         setTimeout(() => {
             resolve(msg);
+            return;
         }, tempo);
     });
 }
 
 // then só vai ser executado depois do "resolve" e o catch só depois do "reject"
-esperar('Frase 1', rand(1, 3))
-    .then(resposta => {
-        console.log(resposta);
-        return esperar('Frase 2', rand(1, 3));
+// esperar('Frase 1', rand(1, 3))
+//     .then(resposta => {
+//         console.log(resposta);
+//         return esperar('Frase 2', rand(1, 3));
+//     })
+//     .then(resposta => {
+//         console.log(resposta);
+//         return esperar(2222, rand(1, 3));
+//     })
+//     .then(resposta => {
+//         console.log(resposta);
+//     })
+//     .then(() => {
+//         console.log('Eu serei o último a ser exibido!');
+//     })
+//     .catch(e => {
+//         console.log('Erro:', e)
+//     });
+
+// Promise.all Promise.race Promise.resolve Promise.reject
+const promises = [
+    esperar('Promise 1', rand(1, 5)),
+    esperar('Promise 2', rand(1, 5)),
+    esperar('Promise 3', rand(1, 5)),
+];
+
+// primeira que resolver
+// Promise.race(promises)
+//     .then(function (valor) {
+//         console.log(valor);
+//     })
+//     .catch(function (erro) {
+//         console.log(erro);
+//     });
+
+// resolve tudo
+Promise.all(promises)
+    .then(function (valor) {
+        console.log(valor);
     })
-    .then(resposta => {
-        console.log(resposta);
-        return esperar(2222, rand(1, 3));
-    })
-    .then(resposta => {
-        console.log(resposta);
-    })
-    .then(() => {
-        console.log('Eu serei o último a ser exibido!');
-    })
-    .catch(e => {
-        console.log('Erro:', e)
-    }); 
+    .catch(function (erro) {
+        console.log(erro);
+    });
